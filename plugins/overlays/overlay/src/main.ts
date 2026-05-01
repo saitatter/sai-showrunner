79935611e93dd938841b47ecf395562a46c3a505
SaiTatter <andrei.voicu133@gmail.com>
2026-05-01 21:04:23 +0300

    feat(overlays): add chat feed widget

--- plugins/overlays/overlay/src/main.ts
+++ plugins/overlays/overlay/src/main.ts
@@ -5,8 +5,9 @@ import EmoteBouncer from "./widgets/EmoteBouncer.vue"
 import Alert from "./widgets/Alert.vue"
 import Bar from "./widgets/Bar.vue"
 import LeaderBoard from "./widgets/LeaderBoard.vue"
+import ChatFeed from "./widgets/ChatFeed.vue"
 
 export default definePluginOverlays({
 	id: "overlays",
-	widgets: [LabelVue, EmoteBouncer, Alert, Bar, LeaderBoard],
+	widgets: [LabelVue, EmoteBouncer, Alert, Bar, LeaderBoard, ChatFeed],
 })
