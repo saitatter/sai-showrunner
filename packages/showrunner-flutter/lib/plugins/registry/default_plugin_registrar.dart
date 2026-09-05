cfd7bb626e5c66aac1eb3ba4e45b68853f69b222
Voicu Andrei-Daniel <andrei.voicu133@gmail.com>
2026-09-05 18:12:13 +0300

    refactor(plugins): isolate default registry composition

---
+++ packages/showrunner-flutter/lib/plugins/registry/default_plugin_registrar.dart
@@ -0,0 +1,91 @@
+part of 'plugin_bootstrap.dart';
+
+DartPluginRegistry buildDefaultPluginRegistry({
+  DartPluginEventHub? eventHub,
+  TtsSpeechService? ttsService,
+  SoundOutputRegistry? soundOutputs,
+  ViewerDataRepository? viewerDataRepository,
+}) {
+  final variablesRepository =
+      viewerDataRepository ?? InMemoryViewerDataRepository();
+  final registry = DartPluginRegistry();
+  registry.register(createShowRunnerPlugin());
+  registry.register(createObsPlugin(CallbackObsTransport(_unconfiguredObs)));
+  registry.register(
+    createAitumPlugin(CallbackObsTransport(_unconfiguredAitum)),
+  );
+  registry.register(
+    createAdvssPlugin(
+      CallbackObsTransport(_unconfiguredAitum),
+      eventHub: eventHub,
+    ),
+  );
+  registry.register(
+    createYouTubePlugin(
+      YouTubeTransport(_unconfiguredYouTube),
+      eventHub: eventHub,
+    ),
+  );
+  registry.register(
+    createTwitchPlugin(
+      TwitchTransport(_unconfiguredTwitch),
+      eventHub: eventHub,
+    ),
+  );
+  _registerTwitchStreamPlanComponent(
+    transport: TwitchTransport(_unconfiguredTwitch),
+  );
+  registry.register(
+    createModerationPlugin(
+      ModerationService(
+        dataService: ShowRunnerDataService(Directory.systemTemp),
+      ),
+    ),
+  );
+  registry.register(createDiscordPlugin());
+  registry.register(
+    createBlueskyPlugin(BlueskyTransport(_unconfiguredBluesky)),
+  );
+  registry.register(createDonorDrivePlugin(null, eventHub: eventHub));
+  registry.register(createRemotePlugin(eventHub: eventHub));
+  registry.register(
+    createVoiceModPlugin(
+      CallbackVoiceModTransport(
+        getVoicesCallback: _unconfiguredVoiceModVoices,
+        selectVoiceCallback: _unconfiguredVoiceMod,
+      ),
+    ),
+  );
+  registry.register(
+    createSoundPlugin(ttsService: ttsService, soundOutputs: soundOutputs),
+  );
+  registry.register(createMinecraftPlugin());
+  registry.register(createHttpPlugin(eventHub: eventHub));
+  registry.register(createTimePlugin());
+  registry.register(createOsPlugin());
+  registry.register(createRandomPlugin(eventHub: eventHub));
+  registry.register(
+    createVariablesPlugin(
+      viewerDataRepository: variablesRepository,
+      eventHub: eventHub,
+    ),
+  );
+  registry.register(createOverlaysPlugin(eventHub: eventHub));
+  registry.register(createSpellcastPlugin(eventHub: eventHub));
+  registry.register(createIotPlugin());
+  registry.register(createGoveePlugin(GoveeTransport(_unconfiguredGovee)));
+  registry.register(
+    createPhilipsHuePlugin(HueTransport(_unconfiguredPhilipsHue)),
+  );
+  registry.register(
+    createTwinklyPlugin(TwinklyTransport(_unconfiguredTwinkly)),
+  );
+  registry.register(createElgatoPlugin(ElgatoTransport(_unconfiguredElgato)));
+  registry.register(createKasaPlugin(KasaTransport(_unconfiguredKasa)));
+  registry.register(createLifxPlugin(_unconfiguredLifxTransport));
+  registry.register(createWyzePlugin(_unconfiguredWyzeTransport));
+  registry.register(dashboardPlugin);
+  registry.register(createInputPlugin());
+  registry.register(createStreamPlansPlugin(registry: registry));
+  return registry;
+}
